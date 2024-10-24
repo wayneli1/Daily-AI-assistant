8e9210ea174aa8a5943b272029f69bd6914b900d
ZhengjiaPu <helenap7231@gmail.com>
2024-10-24 23:00:23 +1100

    feat(Intelligent Health Monitoring): Realize the unification of front-end layout and dashboard, and realize health data transmission

---
+++ pages/dashboard/intelligenthealthmonitoring/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HealthMonitoring from '@/components/HealthMonitoring'
+
+const index = () => {
+    return (
+        <div>
+            <HealthMonitoring/>
+        </div>
+    )
+}
+
+export default index
